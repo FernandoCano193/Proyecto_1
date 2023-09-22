e6628e765a92a14e20280e4e1df7467382ba16e5
Fernando Cano <jose.cano193@tectijuana.edu.mx>
2023-09-22 17:09:11 +0000

    Muestra los datos de los libros en html

--- webserver.py
+++ webserver.py
@@ -5,51 +5,43 @@ from urllib.parse import parse_qsl, urlparse
 import re
 import redis 
 
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
 class WebRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        
         self.url = urlparse(self.path)
 
-        if self.path == '/':
+        if self.url.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b'<html><body>')
-            self.wfile.write(b'<h1>Lista de Libros</h1>')
-            keys = r.keys('libro*')
-            print(keys)
-            
-            self.wfile.write(b'<ul>')
-            for book in keys:
-                datos = r.hgetall(book)
-                print(datos)
-                titulo = datos.get('titulo')
-                print(titulo)
-##                self.wfile.write(f'<li>{titulo}</li>')
-            self.wfile.write(b'</ul>')
+            self.wfile.write(self.get_html().encode('utf-8'))
 
-        book_id = re.findall(r'^/Book/(\d+)',self.url.path)
-
-
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-        self.wfile.write(self.get_response(book_id).encode("utf-8"))
-
-    def get_response(self, book_id):
-            if book_id:
-                if book_id[0] in books:
-                    return f"""
-                    <h1> Hola Web </h1>
-                    <p>  {books[book_id[0]]}   </p>
-                    """
-            return f"""
-        <h1> Hola Web </h1>
-        <p>  {self.path}   </p>
+    def get_html(self):
+        libros=r.keys('libro*')
+        print(libros)
+        html = """<!DOCTYPE html>
+    <html lang="es-mx">
+    <head>
+        <meta charset="UTF-8">
+        <title>Lista de Libros</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>Lista de Libros</h1>
+        <ul>
+        """
+        for libro_id in libros:
+                libro = r.hgetall(libro_id)
+                html += f"<li><strong>Título:</strong> {libro.get('titulo')}<br>"
+                html += f"<strong>Autor:</strong> {libro.get('autor')}<br>"
+                html += """
+        </ul>
+    </body>
+    </html>
     """
+        return html
 
 books = {'1':'Harry',
          '2':'Potter'}
