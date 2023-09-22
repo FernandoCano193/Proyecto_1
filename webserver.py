b65652c86ca9779ae2addf40cd15e991131a9ae0
Fernando Cano <jose.cano193@tectijuana.edu.mx>
2023-09-22 15:30:11 +0000

    modified webserver.py

--- webserver.py
+++ webserver.py
@@ -2,49 +2,57 @@ from functools import cached_property
 from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+import re
+import redis 
 
-# Código basado en:
-# https://realpython.com/python-http-server/
-# https://docs.python.org/3/library/http.server.html
-# https://docs.python.org/3/library/http.cookies.html
-
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 class WebRequestHandler(BaseHTTPRequestHandler):
-    @cached_property
-    def url(self):
-        return urlparse(self.path)
-
-    @cached_property
-    def query_data(self):
-        return dict(parse_qsl(self.url.query))
-
-    @cached_property
-    def post_data(self):
-        content_length = int(self.headers.get("Content-Length", 0))
-        return self.rfile.read(content_length)
-
-    @cached_property
-    def form_data(self):
-        return dict(parse_qsl(self.post_data.decode("utf-8")))
-
-    @cached_property
-    def cookies(self):
-        return SimpleCookie(self.headers.get("Cookie"))
 
     def do_GET(self):
+        
+        self.url = urlparse(self.path)
+
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'<html><body>')
+            self.wfile.write(b'<h1>Lista de Libros</h1>')
+            keys = r.keys('libro*')
+            print(keys)
+            
+            self.wfile.write(b'<ul>')
+            for book in keys:
+                datos = r.hgetall(book)
+                print(datos)
+                titulo = datos.get('titulo')
+                print(titulo)
+##                self.wfile.write(f'<li>{titulo}</li>')
+            self.wfile.write(b'</ul>')
+
+        book_id = re.findall(r'^/Book/(\d+)',self.url.path)
+
+
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        self.wfile.write(self.get_response(book_id).encode("utf-8"))
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p>  {self.path}         </p>
-    <p>  {self.headers}      </p>
-    <p>  {self.cookies}      </p>
-    <p>  {self.query_data}   </p>
-"""
+    def get_response(self, book_id):
+            if book_id:
+                if book_id[0] in books:
+                    return f"""
+                    <h1> Hola Web </h1>
+                    <p>  {books[book_id[0]]}   </p>
+                    """
+            return f"""
+        <h1> Hola Web </h1>
+        <p>  {self.path}   </p>
+    """
+
+books = {'1':'Harry',
+         '2':'Potter'}
 
 
 if __name__ == "__main__":
